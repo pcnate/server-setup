7a3cfce9990be6741f9337bfd52620cecae1bbfc
Nathan Baker <pcnate@gmail.com>
2017-07-18 08:18:18 -0500

    Create la.sh

---
+++ bin/la.sh
@@ -0,0 +1,12 @@
+
+#! /bin/sh
+#
+# author: Nathan Baker (unr34l-dud3)
+# date: July 18th, 2017
+# desc: I only like ls when I can see details
+
+clear;
+echo -n "Directory of: ";
+pwd;
+echo;
+ls --color=always -lah
