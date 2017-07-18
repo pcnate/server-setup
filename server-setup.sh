c31b85f850ced31a8e3c3e2f0de87a76effb570b
Nathan Baker <pcnate@gmail.com>
2017-07-18 08:26:02 -0500

    add la script instead of using alias in .bashrc

--- server-setup.sh
+++ server-setup.sh
@@ -1,35 +1,26 @@
 
-# add la alias
-LA_COMMAND="alias la='clear;echo -n \"Directory of: \";pwd;echo;ls --color=always -lah'"
+#! /bin/sh
+#
+# author: Nathan Baker (unr34l-dud3)
+# date: June 15th, 2017
+# desc: setup users, install packages and add some scripts
 
-if grep -Fxq "$LA_COMMAND" /root/.bashrc
-then :
-else
-	echo "$LA_COMMAND" >> /root/.bashrc
-fi
-
-if grep -Fxq "$LA_COMMAND" /etc/skel/.bashrc
-then :
-else
-	echo "$LA_COMMAND" >> /etc/skel/.bashrc
-fi
-
-# create the apt-update script
+# grab scripts
 wget https://raw.githubusercontent.com/dpndbl/server-setup/master/bin/apt-update.sh -O /bin/apt-update
 wget https://raw.githubusercontent.com/dpndbl/server-setup/master/bin/ratom.sh -O /usr/local/bin/ratom
+wget https://raw.githubusercontent.com/dpndbl/server-setup/master/bin/la.sh -O /usr/local/bin/la
 
-chmod +x /bin/apt-update
+# allow executing scripts
+chmod +x /usr/local/bin/apt-update
 chmod +x /usr/local/bin/ratom
+chmod +x /usr/local/bin/la
 
+# update ad install some basic packages
 apt-get update
-
 apt-get dist-upgrade -y
-
 apt-get install htop bwm-ng sudo wget apt-show-versions fail2ban libpam-systemd dbus screen -y
 
-
 # create users
-
 while read p; do
         USER=$( echo "${p}" | egrep -o '[a-z]+@' | sed -n 's/@//p' )
 
